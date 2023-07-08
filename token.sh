fc123270caab04da53676c53bbfceaf9c67bd605
jrzvnn <jrzvnn@gmail.com>
2023-07-09 00:09:17 +0800

    Initial commit

---
+++ token.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Check if xclip is installed
+if ! command -v xclip &> /dev/null; then
+    echo "Error: xclip is not installed. Please install it to use this script."
+    exit 1
+fi
+
+# Check if the file exists
+if [ ! -f "/home/jrzvnn/.keys/.token" ]; then
+    echo "Error: .token.txt file not found."
+    exit 1
+fi
+
+# Read the file and copy the content to clipboard
+content=$(cat "/home/jrzvnn/.keys/.token")
+echo -n "$content" | xclip -selection clipboard
+
+echo "Content copied to clipboard."
+
